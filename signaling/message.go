8e135cea3a3990d126ac9ee367c3efe4cda2a4ed
Jason <jason@zgwit.com>
2024-04-03 11:20:01 +0800

    信令服务器差不多了

--- signaling/message.go
+++ signaling/message.go
@@ -1,8 +1,10 @@
 package signaling
 
 import (
+	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
-	"log"
+	"github.com/zgwit/iot-master/v4/lib"
+	"github.com/zgwit/iot-master/v4/pkg/log"
 )
 
 type Message struct {
@@ -19,7 +21,7 @@ type Client struct {
 
 type Device struct {
 	ws      *websocket.Conn
-	clients map[string]*Client
+	clients lib.Map[Client] //clients map[string]*Client
 }
 
 func (s *Device) transport() {
@@ -27,15 +29,17 @@ func (s *Device) transport() {
 		var msg Message
 		err := s.ws.ReadJSON(&msg)
 		if err != nil {
-			log.Println(err)
+			log.Error(err)
 			break
 		}
 		//转发
 		if msg.Client != "" {
-			if c, ok := s.clients[msg.Client]; ok {
-				err := c.ws.WriteJSON(msg)
+			client := s.clients.Load(msg.Client)
+			if client != nil {
+				err = client.ws.WriteJSON(msg)
 				if err != nil {
-					_ = c.ws.Close()
+					log.Error(err)
+					_ = client.ws.Close()
 				}
 			}
 			continue
@@ -50,24 +54,44 @@ func (s *Device) transport() {
 }
 
 type Server struct {
-	devices map[string]*Device
+	devices lib.Map[Device]
 }
 
 func (s *Server) Register(id string, ws *websocket.Conn) {
-	if s.devices == nil {
-		s.devices = make(map[string]*Device)
-	}
-	if dev, ok := s.devices[id]; ok {
+	dev := s.devices.Load(id)
+	if dev != nil {
 		dev.ws = ws
 		go dev.transport()
 	} else {
-		s.devices[id] = &Device{ws: ws}
+		dev = &Device{ws: ws}
+		s.devices.Store(id, dev)
 	}
 }
 
 func (s *Server) Connect(id string, ws *websocket.Conn) {
-	if s.devices == nil {
-		s.devices = make(map[string]*Device)
+	dev := s.devices.Load(id)
+	if dev == nil {
+		log.Error("device not exits ", id)
+		return
 	}
-	s.devices[id] = &Device{ws: ws}
+
+	cid := uuid.NewString()
+	log.Info("connect ", id, cid)
+
+	for {
+		var msg Message
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Error(err)
+			break
+		}
+
+		msg.Client = cid
+		err = dev.ws.WriteJSON(msg)
+		if err != nil {
+			log.Error(err)
+			break
+		}
+	}
+	log.Info("finished ", id, cid)
 }
